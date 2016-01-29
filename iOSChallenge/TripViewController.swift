4ce942eb477af7349ef19e6aeb1d359f92830161
SMaraia <selio@me.com>
2016-01-29 16:13:04 -0500

    Sending Data to detail view works

--- iOSChallenge/TripViewController.swift
+++ iOSChallenge/TripViewController.swift
@@ -11,10 +11,42 @@ import UIKit
 class TripViewController: UIViewController {
     var trip: Trip?
     
+    @IBOutlet weak var TextView: UITextView!
+    @IBOutlet weak var TripImageOne: UIImageView!
+    @IBOutlet weak var TripImageTwo: UIImageView!
+    @IBOutlet weak var TripName: UILabel!
+    
     func configureView() {
         if let tripUnwrap: Trip = self.trip {
+            TripName.text = tripUnwrap.name
+            if tripUnwrap.tripDescription != "" {
+                TextView.text = tripUnwrap.tripDescription
+            } else {
+                TextView.text = "Fill in your Thoughts Here!"
+            }
+            if tripUnwrap.tripPhotos.count > 0 {
+                TripImageOne.image = tripUnwrap.tripPhotos[0]
+            } else {
+                TripImageOne.tintColor = UIColor.blackColor()
+            }
             
+            if tripUnwrap.tripPhotos.count > 1 {
+                TripImageTwo.image = tripUnwrap.tripPhotos[1]
+            } else {
+                TripImageTwo.tintColor = UIColor.blackColor()
+            }
         }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configureView()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
