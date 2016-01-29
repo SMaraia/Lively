b083d972f1bccbb83da8acf7c8deff0b9d7d1cad
SMaraia <selio@me.com>
2016-01-28 20:40:38 -0500

    Initial Commit

---
+++ iOSChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSChallenge
+//
+//  Created by Sean Maraia on 1/28/16.
+//  Copyright © 2016 Sean Maraia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
