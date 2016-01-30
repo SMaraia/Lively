c9e8bca3ad7fb5a639e492852309cca175c4791a
SMaraia <selio@me.com>
2016-01-29 19:56:33 -0500

    Onward to tvOS

--- iOSChallenge/PhotoViewController.swift
+++ iOSChallenge/PhotoViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class PhotoViewController: UIViewController {
-
+    @IBOutlet weak var photoCollection: UICollectionView?
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        flowLayout?.itemSize = CGSize(width: self.view.frame.width / 3, height: self.view.frame.width / 3)
+        for image in trips.trips{
+            
+        }
+        //photoCollection?.collectionViewLayout.
         // Do any additional setup after loading the view, typically from a nib.
     }
 
