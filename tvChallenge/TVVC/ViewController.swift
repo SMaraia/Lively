fc728030dcc987f9e018edc5bbadfd2163dac4cb
SMaraia <selio@me.com>
2016-01-31 15:42:53 -0500

    ?

---
+++ tvChallenge/TVVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvChallenge
+//
+//  Created by Sean Maraia on 1/29/16.
+//  Copyright © 2016 Sean Maraia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
