40af496a8f59b257d6a9ce7212ebb2c697034fcc
SMaraia <selio@me.com>
2016-01-31 12:05:30 -0500

    icons!!!!

---
+++ iOSChallenge/tvTripTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  tvTripTableViewController.swift
+//  iOSChallenge
+//
+//  Created by Sean Maraia on 1/31/16.
+//  Copyright © 2016 Sean Maraia. All rights reserved.
+//
+
+import Cocoa
+
+class tvTripTableViewController: UITableViewController {
+
+}
