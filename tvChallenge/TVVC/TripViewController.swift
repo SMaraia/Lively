6f50307f355563b784ed60b72706f358a8504537
SMaraia <selio@me.com>
2016-01-31 23:16:55 -0500

    tvOS finishing

--- tvChallenge/TVVC/TripViewController.swift
+++ tvChallenge/TVVC/TripViewController.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 class TripViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tripLabel: UILabel!
+    @IBOutlet weak var tripImage: UIImageView!
     var tripIndex: Int?
+    var trip: Trip?
+    var currentIndex: Int = 0
+    
+    override func viewDidLoad() {
+        trip = trips[tripIndex!]
+        var images : [UIImage] = []
+        for moment in (trip?.moments)! {
+            images.append(moment.image)
+        }
+        tripImage.animationImages = images
+        tripImage.animationDuration = Double(images.count) * 8.0
+        tripImage.startAnimating()
+        
+        tripLabel.text = trip?.moments[currentIndex].journalLog
+        
+        let timer = NSTimer(timeInterval: 8.0, target: self, selector: "updateLabel:", userInfo: nil, repeats: true)
+        NSRunLoop.mainRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+        
+        
+    }
+    
+    func updateLabel(sender: AnyObject?) {
+        currentIndex = (currentIndex + 1) % (trip?.moments.count)!
+        tripLabel.text = trip?.moments[currentIndex].journalLog
+    }
 }
